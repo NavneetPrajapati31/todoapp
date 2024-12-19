504ff20904d605f1072fbda75fbfc43af031a3fa
Navneet Prajapati <navneetprajapati46@gmail.com>
2024-12-19 16:13:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import Todo from "./components/Todo";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
