215f293b90e1c36386dd2ddbc96ffc3999fcc740
Navneet Prajapati <144175174+NavneetPrajapati31@users.noreply.github.com>
2024-12-19 16:25:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from "./components/header";
-import Todo from "./components/Todo";
+import Header from "../components/header";
+import Todo from "../components/Todo";
 import "./App.css";
 
 function App() {
